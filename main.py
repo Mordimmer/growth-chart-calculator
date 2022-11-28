5e0db9502eb0dd4c1be58e87b5dc6edf39b9961c
Mordimmer <kamil.mycka@o2.pl>
2022-11-28 18:48:07 +0100

    First tests with form

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
-from bmi import *
+import tkinter as tk
 
-print("W celu wyznaczenia BMI dziecka podaj następujące wartości")
-x=False
-while x==False:
-    print(x)
-    waga=80#float(input("Waga[kg]: "))
-    wzrost=180#float(input("Wzorst[cm] ")) 
-    wiek=22#float(input("Wiek: "))
-    #płeć=float(input())
-    #x=is_true()
-    x=input("Czy wprowadzone dane są prawidłowe?[t/n]")
-    if x=="t":
-        x = True
-    elif x=="n":
-        x = False
-    else:
-        print("Coś nie tak")
-        x= False
+main = tk.Tk()
+main.title("Kalkulator centylowy")
+
+# get screen width and height
+width = main.winfo_screenwidth()
+height = main.winfo_screenheight()
+
+# full screen
+# main.geometry(f"{width}x{height}")
+main.geometry("1280x720")
+
+# set background color
+main.configure(bg="#000000")
+
+# draw input fields
+tk.Label(main, text="Wzrost [cm]:", bg="#000000", fg="#ffffff").grid(row=0, column=0)
+height_input = tk.Entry(main)
+height_input.grid(row=1, column=0)
+
+tk.Label(main, text="Waga [kg]:", bg="#000000", fg="#ffffff").grid(row=2, column=0)
+weight_input = tk.Entry(main)
+weight_input.grid(row=3, column=0)
+
+tk.Label(main, text="Wiek [miesiące]:", bg="#000000", fg="#ffffff").grid(row=4, column=0)
+age_input = tk.Entry(main)
+age_input.grid(row=5, column=0)
 
 
-bmi = oblicz_bmi(waga, wzrost)
-print(co_wskazuje_bmi(bmi))
+# append values from input fields to file, each value separated by comma
+def append_data():
+    with open("data.txt", "a") as file:
+        file.write(f"\n{height_input.get()}\t{weight_input.get()}\t{age_input.get()}")
+
+
+# button to append data to file
+tk.Button(main, text="Dodaj dane", command=append_data).grid(row=6, column=0)
+
+
+# get data from file, and save it to list, first line is height, second is weight, third is age
+def get_data():
+    with open("data.txt", "r") as file:
+        data = file.readlines()
+        data = [x.strip() for x in data]
+        return data
+
+
+print(get_data())
+
+# draw button
+
+main.mainloop()
