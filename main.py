70d0798d5da778415addb8a3f5f4e516b7e41ec2
Mordimmer <kamil.mycka@o2.pl>
2022-11-29 11:01:17 +0100

    Raio buttons dont't work

--- main.py
+++ main.py
@@ -12,42 +12,39 @@ height = main.winfo_screenheight()
 main.geometry("1280x720")
 
 # set background color
-main.configure(bg="#000000")
-
-# draw input fields
-tk.Label(main, text="Wzrost [cm]:", bg="#000000", fg="#ffffff").grid(row=0, column=0)
-height_input = tk.Entry(main)
-height_input.grid(row=1, column=0)
-
-tk.Label(main, text="Waga [kg]:", bg="#000000", fg="#ffffff").grid(row=2, column=0)
-weight_input = tk.Entry(main)
-weight_input.grid(row=3, column=0)
-
-tk.Label(main, text="Wiek [miesiące]:", bg="#000000", fg="#ffffff").grid(row=4, column=0)
-age_input = tk.Entry(main)
-age_input.grid(row=5, column=0)
+main.configure(bg="#FFCC99")
 
 
-# append values from input fields to file, each value separated by comma
 def append_data():
-    with open("data.txt", "a") as file:
-        file.write(f"\n{height_input.get()}\t{weight_input.get()}\t{age_input.get()}")
+    with open("test.txt", "a") as file:
+        file.write(f"\n{age_input.get()}")
 
 
-# button to append data to file
-tk.Button(main, text="Dodaj dane", command=append_data).grid(row=6, column=0)
+tk.Label(main, text="Kalkulator centylowy", bg="#FFCC99",font=("Arial", 30)).place(x=50, y=20)
+
+tk.Label(main, text="Wiek [miesiące]:", bg="#FFCC99").place(x=50, y=100)
+age_input = tk.Entry(main)
+age_input.place(x=200, y=100)
+
+# radio buttons
+tk.Label(main, text="Płeć dziecka:", bg="#FFCC99").place(x=50, y=150)
+tk.Radiobutton(main, text="Dziewczynka", bg="#FFCC99", activebackground="#ffa64d", value=0).place(x=190, y=150)
+tk.Radiobutton(main, text="Chłopiec", bg="#FFCC99", activebackground="#ffa64d", value=0).place(x=305, y=150)
 
 
-# get data from file, and save it to list, first line is height, second is weight, third is age
-def get_data():
-    with open("data.txt", "r") as file:
-        data = file.readlines()
-        data = [x.strip() for x in data]
-        return data
+tk.Label(main, text="Wzrost [cm]:", bg="#FFCC99").place(x=50, y=200)
+height_input = tk.Entry(main)
+height_input.place(x=200, y=200)
 
+tk.Label(main, text="Waga [kg]:", bg="#FFCC99").place(x=50, y=250)
+weight_input = tk.Entry(main)
+weight_input.place(x=200, y=250)
 
-print(get_data())
+tk.Label(main, text="Obwód głowy [cm]:", bg="#FFCC99").place(x=50, y=300)
+head_input = tk.Entry(main)
+head_input.place(x=200, y=300)
 
-# draw button
+tk.Button(main, text="Zapisz dane", command=append_data).place(x=50, y=350)
+
 
 main.mainloop()
