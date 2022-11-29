8567a1e3dfd95140555c4d776037cd9b9538bfdb
Mordimmer <kamil.mycka@o2.pl>
2022-11-29 13:58:09 +0100

    Working radio buttons

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+# TODO
+# Zmenić tło przycisków
+# Sprawdzić, czy dane są poprawne
 
 main = tk.Tk()
 main.title("Kalkulator centylowy")
@@ -17,20 +20,26 @@ main.configure(bg="#FFCC99")
 
 def append_data():
     with open("test.txt", "a") as file:
-        file.write(f"\n{age_input.get()}")
+        file.write(f"\n{age_input.get()},{selected_sex.get()}")
 
+
+selected_sex = tk.StringVar()
+sex = (("Dziewczynka", "girl"),
+       ("Chłopiec", "boy"))
 
 tk.Label(main, text="Kalkulator centylowy", bg="#FFCC99",font=("Arial", 30)).place(x=50, y=20)
+
+
 
 tk.Label(main, text="Wiek [miesiące]:", bg="#FFCC99").place(x=50, y=100)
 age_input = tk.Entry(main)
 age_input.place(x=200, y=100)
 
-# radio buttons
-tk.Label(main, text="Płeć dziecka:", bg="#FFCC99").place(x=50, y=150)
-tk.Radiobutton(main, text="Dziewczynka", bg="#FFCC99", activebackground="#ffa64d", value=0).place(x=190, y=150)
-tk.Radiobutton(main, text="Chłopiec", bg="#FFCC99", activebackground="#ffa64d", value=0).place(x=305, y=150)
 
+tk.Label(main, text="Płeć dziecka:", bg="#FFCC99").place(x=50, y=150)
+
+tk.Radiobutton( main, text="Dziewczynka", value="girl", variable=selected_sex ).place(x=200, y=150)
+tk.Radiobutton( main, text="Chłopczyk", value="boy", variable=selected_sex ).place(x=320, y=150)
 
 tk.Label(main, text="Wzrost [cm]:", bg="#FFCC99").place(x=50, y=200)
 height_input = tk.Entry(main)
@@ -44,7 +53,5 @@ tk.Label(main, text="Obwód głowy [cm]:", bg="#FFCC99").place(x=50, y=300)
 head_input = tk.Entry(main)
 head_input.place(x=200, y=300)
 
-tk.Button(main, text="Zapisz dane", command=append_data).place(x=50, y=350)
-
-
+tk.Button(main, text="Zapisz dane", command=lambda: [append_data, print(selected_sex.get())]).place(x=50, y=350)
 main.mainloop()
