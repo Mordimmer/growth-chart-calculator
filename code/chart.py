9e841fad2c22995d65525408f986a475fc59ac14
M0rdimmer <kamil.mycka@o2.pl>
2023-03-25 15:57:52 +0100

    !!! Proste siatki centylowe dla wszystkich obranych przez nas wartości

--- code/chart.py
+++ code/chart.py
@@ -38,7 +38,6 @@ def create_charts(main):
         height_95th = centyle_height.iloc[:, 8]
         height_98th = centyle_height.iloc[:, 9]
 
-
         centyle_weight = pd.read_csv("centyle/b_weight.csv")
         age_centyl_weight = centyle_weight.iloc[:, 0]
         weight_2nd = centyle_weight.iloc[:, 1]
@@ -50,6 +49,18 @@ def create_charts(main):
         weight_90th = centyle_weight.iloc[:, 7]
         weight_95th = centyle_weight.iloc[:, 8]
         weight_98th = centyle_weight.iloc[:, 9]
+
+        centyle_headc = pd.read_csv("centyle/b_headc.csv")
+        age_centyl_headc = centyle_headc.iloc[:, 0]
+        headc_2nd = centyle_headc.iloc[:, 1]
+        headc_5th = centyle_headc.iloc[:, 2]
+        headc_10th = centyle_headc.iloc[:, 3]
+        headc_25th = centyle_headc.iloc[:, 4]
+        headc_50th = centyle_headc.iloc[:, 5]
+        headc_75th = centyle_headc.iloc[:, 6]
+        headc_90th = centyle_headc.iloc[:, 7]
+        headc_95th = centyle_headc.iloc[:, 8]
+        headc_98th = centyle_headc.iloc[:, 9]
 
         # create 3 plots
         # (3, 1, 1) means 3 rows, 1 column, 1st plot
@@ -76,7 +87,6 @@ def create_charts(main):
         # show legend next to plot
         ax1.legend(loc="center left", bbox_to_anchor=(1, 0.5), prop={'size': 8})
 
-
         # plot age and weight
         ax2.plot(age, weight, color="black", label="Waga")
         ax2.plot(age_centyl_weight, weight_5th, 'r--', label="5th")
@@ -89,7 +99,7 @@ def create_charts(main):
         ax2.set_xlim(right=max(age))
         ax2.set_ylim(bottom=min(weight) - 3)
         ax2.set_ylim(top=max(weight) + 3)
-        ax2.set_title("Wzrost")
+        ax2.set_title("Waga")
         ax2.set_xlabel("Wiek [miesiące]:")
         ax2.set_ylabel("Waga [kg]")
         ax2.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
@@ -97,14 +107,33 @@ def create_charts(main):
         ax2.legend(loc="center left", bbox_to_anchor=(1, 0.5), prop={'size': 8})
 
         # plot age and head circumference
-        ax3.plot(age, head, color="green")
+        ax3.plot(age, head, color="black", label="Obwód głowy")
+        ax3.plot(age_centyl_headc, headc_5th, 'r--', label="5th")
+        ax3.plot(age_centyl_headc, headc_25th, 'g--', label="25th")
+        ax3.plot(age_centyl_headc, headc_50th, 'b-.', label="50th")
+        ax3.plot(age_centyl_headc, headc_75th, 'g--', label="75th")
+        ax3.plot(age_centyl_headc, headc_95th, 'r--', label="95th")
         ax3.grid(linestyle="--")
+        ax3.set_xlim(left=0)
+        ax3.set_xlim(right=max(age))
+        ax3.set_ylim(bottom=min(head) - 3)
+        ax3.set_ylim(top=max(head) + 3)
         ax3.set_title("Obwód głowy")
-        ax3.set_xlabel("Wiek [miesiące]")
+        ax3.set_xlabel("Wiek [miesiące]:")
         ax3.set_ylabel("Obwód głowy [cm]")
-        ax3.set_ylim(bottom=0)
         ax3.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
-        ax3.set_ylim(top=ax3.get_ylim()[1] * 1.2)
+        # show legend next to plot
+        ax3.legend(loc="center left", bbox_to_anchor=(1, 0.5), prop={'size': 8})
+
+        # # plot age and head circumference
+        # ax3.plot(age, head, color="green")
+        # ax3.grid(linestyle="--")
+        # ax3.set_title("Obwód głowy")
+        # ax3.set_xlabel("Wiek [miesiące]")
+        # ax3.set_ylabel("Obwód głowy [cm]")
+        # ax3.set_ylim(bottom=0)
+        # ax3.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
+        # ax3.set_ylim(top=ax3.get_ylim()[1] * 1.2)
 
         # improve layout
         plt.tight_layout()
